a4d6f0725f1ecc069350d2643e1305eff759e7a6
pitcany <pitcany@gmail.com>
2015-11-24 18:50:12 -0800

    Update spectralHMM.m

--- final_project/spectralHMM.m
+++ final_project/spectralHMM.m
@@ -3,19 +3,38 @@ Close = data.Close;
 obs = Close.data;
 
 % pulls in SP500 data in using quandl
+interval_len=floor(length(data)/10);
 
-tenday=zeros(125,1);
-for i=1:125
+agg_returns=zeros(interval_len,1);
+for i=1:interval_len
     j=10*i-9;
-    tenday(i)=prod(1+obs(j:(j+9)));
+    agg_returns(i)=prod(1+obs(j:(j+9)));
 end
 
-tenday = tenday - 1;
+clear i j;
+
+agg_returns = agg_returns - 1;
 % the above code generates a list of rates of increase for 10 day
 % intervals. do this to avoid having really small observation values
 
-timestep = (1:length(tenday))';
-hmm = [timestep HMM_obs(tenday,6)];
-tenday = [timestep tenday];
+% num_discrete_obs is the number of discrete observations
+num_discrete_obs = 6;
+
+timestep = (1:interval_len)';
+hmm = [timestep HMM_obs(agg_returns,num_discrete_obs)];
+agg_returns = [timestep agg_returns];
 
 %create empirical estimates of P1, P21, P3_x_1
+%first randomly sample observation triples
+%num_samples is the number of samples
+num_samples = 100;
+%also we truncate our observation array by the last two elements in order
+%to sample the end triple
+indices_to_sample = timestep(1:end-2);
+
+sampled_indices=randsample(indices_to_sample,num_samples,true);
+triples=zeros(num_samples,3);
+for i=1:num_samples
+    j=sampled_indices(i);
+    triples(i,:)=hmm(j:j+2,2);
+end
