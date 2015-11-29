2a697c19777130c4643625eafed9537cf1ed043c
pitcany <pitcany@gmail.com>
2015-11-29 09:55:46 -0800

    Create quandl_authorize.m

---
+++ final_project/updated_spectral_HMM/quandl_authorize.m
@@ -0,0 +1,5 @@
+Quandl.auth('ZL-ZtyRkTD1KEkZSQycg');
+clear ans;
+%Authorize use of Quandl
+
+%[data, headers] = Quandl.get('YAHOO/INDEX_GSPC','start_date','2010-11-23','end_date','2015-11-23','transformation','rdiff');
